71e25f0a42eefa1567b1b3ab613852c5b9496c11
Tanish Yadav <tanishyadav.jpr@gmail.com>
2022-05-17 21:48:43 +0530

    ZSH: Remove public ip from prompt

--- p10k.zsh
+++ p10k.zsh
@@ -96,8 +96,8 @@
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
-    ram                   # free RAM
-    swap                  # used swap
+    # public_ip             # public IP address
+    battery                 # internal battery
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
@@ -105,9 +105,9 @@
     # =========================[ Line #2 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
-    public_ip             # public IP address
+    ram                     # free RAM
+    swap                    # used swap
     # proxy                 # system-wide http/https/ftp proxy
-    battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
