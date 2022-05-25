83d056dbe681a5a46c81b0d6c3ea170c6e03a22a
Tanish Yadav <tanishyadav.jpr@gmail.com>
2022-05-25 22:02:57 +0530

    INSTME: Reformat install-zsh.sh

--- installme/install-zsh.sh
+++ installme/install-zsh.sh
@@ -4,32 +4,28 @@
 
 source "$(dirname $(readlink -f $0))/config.sh"
 
-ZSH_SRC_NAME=$SRC_PATH/zsh.tar.xz
-ZSH_PACK_DIR=$SRC_PATH/zsh
-ZSH_LINK="https://sourceforge.net/projects/zsh/files/latest/download"
+ZSH_SRC_AC_NAME=$SRC_PATH/zsh.tar.xz
+ZSH_SRC_PATH=$SRC_PATH/zsh
+ZSH_AC_LINK="https://sourceforge.net/projects/zsh/files/latest/download"
 
-if [[ -d "$ZSH_PACK_DIR" ]]; then
+if [[ -d "$ZSH_SRC_PATH" ]]; then
 	echo "Removing already existing zsh directory..."
-	rm -rf "$ZSH_PACK_DIR"
+	rm -rf "$ZSH_SRC_PATH"
 fi
 
-if [[ ! -d "$ZSH_PACK_DIR" ]]; then
-	echo "Creating zsh directory..."
-	mkdir -p "$ZSH_PACK_DIR"
-fi
+echo "Creating zsh directory..."
+mkdir -p "$ZSH_SRC_PATH"
 
-if [[ -f $ZSH_SRC_NAME ]]; then
+if [[ -f $ZSH_SRC_AC_NAME ]]; then
 	echo "Removing already existing zsh source archive..."
-	rm -f $ZSH_SRC_NAME
+	rm -f $ZSH_SRC_AC_NAME
 fi
 
-if [[ ! -f $ZSH_SRC_NAME ]]; then
-	echo "Downloading zsh source archive from $ZSH_LINK"
-	curl -Lo "$ZSH_SRC_NAME" "$ZSH_LINK"
-fi
+echo "Downloading zsh source archive from $ZSH_AC_LINK"
+curl -Lo "$ZSH_SRC_AC_NAME" "$ZSH_AC_LINK"
 
-tar xJvf "$ZSH_SRC_NAME" -C "$ZSH_PACK_DIR" --strip-components 1
-cd "$ZSH_PACK_DIR"
+tar xJvf "$ZSH_SRC_AC_NAME" -C "$ZSH_SRC_PATH" --strip-components 1
+cd "$ZSH_SRC_PATH"
 
 #./configure --prefix="$HOME/local" \
 #    CPPFLAGS="-I$HOME/local/include" \
