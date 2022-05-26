8cc29719ee408eec3cd0009c4bdfc06e590f5e7c
Tanish Yadav <tanishyadav.jpr@gmail.com>
2022-05-26 19:46:32 +0530

    INSTME: pyenv: FIX

--- installme/install-pyenv.sh
+++ installme/install-pyenv.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+PYENV_PATH="$HOME/.pyenv"
+
+if [[ -d "$PYENV_PATH" ]]; then
+	echo "Removing already existing pyenv directory..."
+	rm -rf "$PYENV_PATH"
+fi
+
 curl https://pyenv.run | bash
