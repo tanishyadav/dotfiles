ee1c3e80181ba007fffd37d6aef12370708d52e2
Tanish Yadav <tanishyadav.jpr@gmail.com>
2022-06-04 18:19:50 +0530

    INSTME: config: Add yesno

--- installme/config.sh
+++ installme/config.sh
@@ -3,3 +3,20 @@
 PREFIX_PATH=$HOME/.local
 SRC_PATH=$PREFIX_PATH/src
 ZSH_PATH=$HOME/.zsh
+
+function yesno() {
+	while true; do
+		read -p "$1" answer
+		case ${answer:0:1} in
+			y|Y )
+				return 0
+				;;
+			n|N )
+				return 1
+				;;
+			* )
+				echo "Invalid response"
+				;;
+		esac
+	done
+}
--- installme/install-zsh.sh
+++ installme/install-zsh.sh
@@ -1,59 +1,50 @@
 #!/usr/bin/env bash
 
-#set -euxo pipefail
-
 source "$(dirname $(readlink -f $0))/config.sh"
 
-ZSH_SRC_AC_NAME=$SRC_PATH/zsh.tar.xz
-ZSH_SRC_PATH=$SRC_PATH/zsh
+ZSH_SRC_AC_NAME="$SRC_PATH/zsh.tar.xz"
+ZSH_SRC_PATH="$SRC_PATH/zsh"
 ZSH_AC_LINK="https://sourceforge.net/projects/zsh/files/latest/download"
 
 if [[ -d "$ZSH_SRC_PATH" ]]; then
-	echo "Removing already existing zsh directory..."
+	echo "Removing already existing zsh source directory..."
 	rm -rf "$ZSH_SRC_PATH"
 fi
 
 echo "Creating zsh directory..."
 mkdir -p "$ZSH_SRC_PATH"
 
-if [[ -f $ZSH_SRC_AC_NAME ]]; then
+if [[ -f "$ZSH_SRC_AC_NAME" ]]; then
 	echo "Removing already existing zsh source archive..."
-	rm -f $ZSH_SRC_AC_NAME
+	rm -f "$ZSH_SRC_AC_NAME"
 fi
 
-echo "Downloading zsh source archive from $ZSH_AC_LINK"
+echo "Downloading zsh source archive..."
 curl -Lo "$ZSH_SRC_AC_NAME" "$ZSH_AC_LINK"
 
+echo "Extracting zsh source archive..."
 tar xJvf "$ZSH_SRC_AC_NAME" -C "$ZSH_SRC_PATH" --strip-components 1
+echo "Removing zsh source archive..."
+rm "$ZSH_SRC_AC_NAME"
 cd "$ZSH_SRC_PATH"
 
-#./configure --prefix="$HOME/local" \
-#    CPPFLAGS="-I$HOME/local/include" \
-#    LDFLAGS="-L$HOME/local/lib"
-
+echo "Running configure..."
 ./configure --prefix="$PREFIX_PATH"
+echo "Running make..."
 make -j && make install
 
-while true; do
-read -p "Do you want to add zsh to /etc/shells? (y/n) " yn
-case $yn in
-	[yY] ) echo "Adding to /etc/shells...";
-		sudo add-shell "$PREFIX_PATH/bin/zsh";
-		break;;
-	[nN] ) echo "NOT adding to /etc/shells";
-		break;;
-	* ) echo "Invalid response";;
-esac
-done
+echo
+echo "---------------------------------------------------------------------"
+echo "                            ZSH BUILT!!!                             "
+echo "---------------------------------------------------------------------"
+echo
 
-while true; do
-read -p "Do you want to change the default shell? (y/n) " yn
-case $yn in
-	[yY] ) echo "Changing default shell...";
-		sudo chsh -s "$PREFIX_PATH/bin/zsh" $USER;
-		break;;
-	[nN] ) echo "NOT chaging the default shell";
-		break;;
-	* ) echo "Invalid response";;
-esac
-done
+if yesno "Do you want to add zsh to /etc/shells? (y/n) "; then
+	echo "Adding to /etc/shells...";
+	sudo add-shell "$PREFIX_PATH/bin/zsh";
+fi
+
+if yesno "Do you want to change the default shell? (y/n) "; then
+	echo "Changing default shell...";
+	sudo chsh -s "$PREFIX_PATH/bin/zsh" $USER;
+fi
