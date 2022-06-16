e4684ec37c810f6c8499f2daa1e38858620b8968
Tanish Yadav <tanishyadav.jpr@gmail.com>
2022-06-16 16:39:13 +0530

    INSTME: Add more utility paths

--- installme/config.sh
+++ installme/config.sh
@@ -4,8 +4,11 @@ TMP_PATH="/tmp"
 PREFIX_PATH="$HOME/.local"
 SRC_PATH="$PREFIX_PATH/src"
 BIN_PATH="$PREFIX_PATH/bin"
+SHARE_PATH="$PREFIX_PATH/share"
+MAN_PATH="$SHARE_PATH/man/man1"
 PYENV_PATH="$HOME/.pyenv"
 ZSH_PATH="$HOME/.zsh"
+ZSH_FUNC_PATH="$SHARE_PATH/zsh/site-functions"
 
 function yesno() {
 	while true; do
